958a3a76c97a14571c375b023aae436ca20b7098
Linux Dabbler <42593036+linuxdabbler@users.noreply.github.com>
2021-07-12 00:26:05 -0500

    update colorscheme and centered

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen*/
+static int centered = 1;                    /* -c option; centers dmenu on screen*/
 static min_width = 900;                     /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
@@ -11,13 +11,13 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#96b5b4", "#282a36" },
+	[SchemeNorm] = { "#5e8d87", "#1d1f21" },
 	[SchemeSel] = { "#d7d7d7", "#4d4d4d" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
-static unsigned int lineheight = 22;         /* -h option; minimum height of a menu line */
+static unsigned int lines      = 10;
+static unsigned int lineheight = 26;         /* -h option; minimum height of a menu line */
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
@@ -25,4 +25,4 @@ static unsigned int lineheight = 22;         /* -h option; minimum height of a m
 static const char worddelimiters[] = " ";
 
 /* Side of window border*/
-static const unsigned int border_width = 1;
+static const unsigned int border_width = 5;
